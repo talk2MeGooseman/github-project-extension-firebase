4e6d9b11127055ad6363dba41c0f710b5039a050
Erik Guzman <me.erik@me.com>
2018-01-31 19:46:05 -0800

    Small bug fixes after refactoring

--- functions/AuthToken.js
+++ functions/AuthToken.js
@@ -1,5 +1,6 @@
 'use strict';Object.defineProperty(exports, "__esModule", { value: true });exports.
 
+
 decodeToken = decodeToken;exports.
 
 
@@ -12,10 +13,10 @@ verifyToken = verifyToken;exports.
 
 
 
-signToken = signToken;var _jsonwebtoken = require('jsonwebtoken');var _jsonwebtoken2 = _interopRequireDefault(_jsonwebtoken);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function decodeToken(token, secret) {const secret_decoded = new Buffer(secret, 'base64');return _jsonwebtoken2.default.verify(token, secret_decoded);}function verifyToken(token, secret) {const decoded = decodeToken(token, secret);if (decoded.role != 'broadcaster') throw 'Must be broadcaster role.';return decoded;}function signToken(secret) {
+signToken = signToken;var _jsonwebtoken = require('jsonwebtoken');var _jsonwebtoken2 = _interopRequireDefault(_jsonwebtoken);var _Constants = require('./Constants');function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function decodeToken(token, secret) {const secret_decoded = new Buffer(secret, 'base64');return _jsonwebtoken2.default.verify(token, secret_decoded);}function verifyToken(token, secret) {const decoded = decodeToken(token, secret);if (decoded.role != 'broadcaster') throw 'Must be broadcaster role.';return decoded;}function signToken(secret) {
   const secret_decoded = new Buffer(secret, 'base64');
   const tokenObj = {
-    "user_id": EXTENSTION_USER_ID,
+    "user_id": _Constants.EXTENSTION_USER_ID,
     "role": "external" };
 
 
--- functions/TwitchAPI.js
+++ functions/TwitchAPI.js
@@ -8,24 +8,18 @@
 
 
 setExtensionConfigured = setExtensionConfigured;var _axios = require("axios");var _axios2 = _interopRequireDefault(_axios);var _Constants = require("./Constants");var _AuthToken = require("./AuthToken");function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}async function setExtensionConfigured(channel_id, secret) {
-  try {
-    const token = (0, _AuthToken.signToken)(secret);
+  const token = (0, _AuthToken.signToken)(secret);
 
-    let response = await (0, _axios2.default)({
-      method: 'PUT',
-      url: `${_Constants.TWITCH_BASE_EXTENSION_URL}/${EXTENSION_ID}/${_Constants.EXTENSION_VERSION}/required_configuration?channel_id=${channel_id}`,
-      data: {
-        "required_configuration": _Constants.CONFIG_KEY },
+  let response = await (0, _axios2.default)({
+    method: 'PUT',
+    url: `${_Constants.TWITCH_BASE_EXTENSION_URL}/${_Constants.EXTENSION_ID}/${_Constants.EXTENSION_VERSION}/required_configuration?channel_id=${channel_id}`,
+    data: {
+      "required_configuration": _Constants.CONFIG_KEY },
 
-      headers: {
-        'Content-Type': 'application/json',
-        'Client-id': EXTENSION_ID,
-        'Authorization': `Bearer ${token}` } });
-
+    headers: {
+      'Content-Type': 'application/json',
+      'Client-id': _Constants.EXTENSION_ID,
+      'Authorization': `Bearer ${token}` } });
 
 
-    return await response.json();
-  } catch (error) {
-    console.log(error);
-  }
 }
