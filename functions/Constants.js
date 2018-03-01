602a8a7e76673472dacccfe3ba5ea8f87c31e460
Erik Guzman <me.erik@me.com>
2018-02-28 20:03:50 -0800

    Update to version 0.0.3

--- functions/Constants.js
+++ functions/Constants.js
@@ -3,7 +3,7 @@ const GITHUB_BASE_URL = exports.GITHUB_BASE_URL = 'https://api.github.com';
 const BROADCASTER_COLLECTION = exports.BROADCASTER_COLLECTION = 'broadcasters';
 const REPOS_COLLECTION = exports.REPOS_COLLECTION = 'repos';
 const TWITCH_BASE_EXTENSION_URL = exports.TWITCH_BASE_EXTENSION_URL = 'https://api.twitch.tv/extensions';
-const EXTENSION_VERSION = exports.EXTENSION_VERSION = '0.0.2';
+const EXTENSION_VERSION = exports.EXTENSION_VERSION = '0.0.3';
 const EXTENSION_ID = exports.EXTENSION_ID = 'yncbd7i177on3ia536r307nlvt8g1w';
 const EXTENSTION_USER_ID = exports.EXTENSTION_USER_ID = '120750024';
 const CONFIG_KEY = exports.CONFIG_KEY = 'config-1';
